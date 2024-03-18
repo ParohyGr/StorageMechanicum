5368f5d4e0be524c689c5f57f50b3f202dbfc5a1
Tomas Paronai <tomas.paronai@goodrequest.com>
2024-03-18 16:05:59 +0100

    init

---
+++ app/src/main/java/com/parohy/storagemechanicum/ReadFromLocal.kt
@@ -0,0 +1,32 @@
+package com.parohy.storagemechanicum
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.mutableStateOf
+import com.parohy.storagemechanicum.databinding.UseCaseBinding
+
+class ReadFromLocal : ComponentActivity() {
+  private val binding by lazy { UseCaseBinding.inflate(layoutInflater) }
+
+  private val pickImageRequest = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+    if (result.resultCode == Activity.RESULT_OK)
+      binding.imageView.setImageURI(result.data?.data)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding.apply {
+      setContentView(root)
+
+      button.setOnClickListener {
+        pickImageRequest.launch(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
+      }
+    }
+  }
+}
